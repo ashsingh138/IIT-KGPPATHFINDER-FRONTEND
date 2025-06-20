46601124b95a486cdfbc87bc4b25abe6f4158196
Ashutosh Singh <ashking1308@gmail.com>
2025-06-20 11:55:57 +0530

    updated

--- src/PathMap.jsx
+++ src/PathMap.jsx
@@ -3,7 +3,7 @@ import { MapContainer, TileLayer, Marker, Polyline, Popup, useMapEvents } from '
 import axios from 'axios';
 import './PathMap.css';
 
-const API_BASE = 'import.meta.env.VITE_API_BASE_URL;';
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
 function MapClickHandler({ onMapClick }) {
   useMapEvents({ click(e) { onMapClick(e.latlng); } });
